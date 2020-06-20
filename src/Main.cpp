279146abfcf14b42b6d7027058e5164fd34ee04c
Josh Kennedy <joshk@digitalmagic.io>
2020-06-19 22:03:49 -0500

    Load the builtins in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 
+#include "BuiltinHandler.hpp"
 #include "REPL.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   std::cout << "Wassup World!" << std::endl;
 
+  BuiltinHandler::getInstance()->loadBuiltins();
+
   REPL::getInstance()->startREPL();
-  
+
   return 0;
 }
